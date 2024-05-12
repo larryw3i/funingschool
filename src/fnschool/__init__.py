69cb015d0d150b16e6abf6bae0860433d38fba46
larryw3i <larryw3i@163.com>
2024-05-13 05:19:10 +0800

    Daily copy.

--- src/fnschool/__init__.py
+++ src/fnschool/__init__.py
@@ -12,7 +12,7 @@ from fnschool.path import *
 from fnschool.entry import *
 from fnschool.external import *
 
-__version__ = "20240513.0500.10"
+__version__ = "20240513.0518.24"
 
 
 def print_app_name():
