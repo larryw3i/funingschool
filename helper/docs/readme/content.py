d15856a357efac504b66d7fdc97c4435210daf47
larryw3i <larryw3i@163.com>
2024-10-14 11:34:51 +0800

    Add meal types.

--- helper/docs/readme/content.py
+++ helper/docs/readme/content.py
@@ -89,7 +89,7 @@ README.md">English</a>
 ![{_("Screenshot")}]({_("https://raw.githubusercontent.com/larryw3i/funingschool/master/"+
 "Documentation/images/9432e132-f8cd-11ee-8ee6-f37309efa64b.png")})
 
-<h2 name="key-features">
+<h2 id="key-features">
     {_("Key Features")}
 </h2>
 
@@ -119,7 +119,7 @@ README.md">English</a>
 * {_("Display comments.")}
 * {_("Effectively assist testers, especially teachers and students.")}
 
-<h2 name="how-to-use">
+<h2 id="how-to-use">
     {_("How To Use")}
 </h2>
 
@@ -156,7 +156,7 @@ fnschool-cli exam enter
     + "which is the key to using it well."
 )}
 
-<h2 name="credits">
+<h2 id="credits">
     {_("Credits")}
 </h2>
 <p>
@@ -172,7 +172,7 @@ fnschool-cli exam enter
     </ul>
 </p>
 
-<h2 name="support">
+<h2 id="support">
     {_("Support")}
 </h2>
 <h3>
@@ -183,7 +183,7 @@ fnschool-cli exam enter
 ({_("https://raw.githubusercontent.com/larryw3i/funingschool/master"+
 "/Documentation/images/9237879a-f8d5-11ee-8411-23057db0a773.jpeg")})
 
-<h2 name="license">
+<h2 id="license">
     {_("License")}
 </h2>
 
