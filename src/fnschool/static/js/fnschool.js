d23d5c70aeb9974420a1826b8745c6a486d6d475
Larry Wei <larryw3i@163.com>
2025-09-26 21:11:46 +0800

    Daily Copy.

--- src/fnschool/static/js/fnschool.js
+++ src/fnschool/static/js/fnschool.js
@@ -1,1 +1,10 @@
-
+function update_href(query) {
+  query = new Map(Object.entries(query));
+  const url = new URL(window.location.href);
+  const params = new URLSearchParams(url.search);
+  for (const [key, value] of query) {
+    params.set(key, value);
+  }
+  url.search = params.toString();
+  window.location.href = url.href;
+}
