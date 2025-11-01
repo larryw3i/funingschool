ab1fab1031f1af9eccd46862433013c70efbb921
Larry Wei <larryw3i@163.com>
2025-11-01 22:56:36 +0800

    Daily Copy.

--- src/fnschoo1/patches/202511012053_copy_profiles_to_fnprofile_data/profiles/apps.py
+++ src/fnschoo1/patches/202511012053_copy_profiles_to_fnprofile_data/profiles/apps.py
@@ -1,3 +1,5 @@
+import importlib
+
 from django.apps import AppConfig
 
 
@@ -5,6 +7,11 @@ class ProfilesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     # name = "profiles"
     name = "patches.202511012053_copy_profiles_to_fnprofile_data.profiles"
+
+    def ready(sef):
+        importlib.import_module(
+            "patches.202511012053_copy_profiles_to_fnprofile_data.profiles.signals"
+        )
 
 
 # The end.
